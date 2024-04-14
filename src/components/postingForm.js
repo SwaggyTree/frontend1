37a9fb8e7ad694be3e202a897b8c14a5e3b44f6e
SwaggyTree <leon_pablo@dlsu.edu.ph>
2024-04-15 02:10:34 +0800

    hehe

--- src/components/postingForm.js
+++ src/components/postingForm.js
@@ -1,82 +1,75 @@
-import { useState } from 'react'
-import { usePostsContext } from '../hooks/usePostsContext'
+import { useState } from "react";
+import { usePostsContext } from "../hooks/usePostsContext";
 
 const PostingForm = () => {
-    const{ dispatch } = usePostsContext()
-    const [title, setTitle] = useState('')
-    const [user, setUser] = useState('')
-    const [tag, setTag] = useState('')
-    const [body, setBody] = useState('')
-    const [error, setError] = useState(null)
+  const { dispatch } = usePostsContext();
+  const [title, setTitle] = useState("");
+  const [user, setUser] = useState("");
+  const [tag, setTag] = useState("");
+  const [body, setBody] = useState("");
+  const [error, setError] = useState(null);
 
-    const formSubmit = async (e) => {
-        e.preventDefault()
+  const formSubmit = async (e) => {
+    e.preventDefault();
 
-        const post = {title, user, tag, body}
+    const post = { title, user, tag, body };
 
-        const response = await fetch('https://compiled-mern-hszc.vercel.app/api/posts', {
-            method: 'POST',
-            body: JSON.stringify(post),
-            headers: {
-                'Content-type': 'application/json'
-            }
-        })
-        const json = await response.json()
+    const response = await fetch("https://poggers-drab.vercel.app/api/posts", {
+      method: "POST",
+      body: JSON.stringify(post),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const json = await response.json();
 
-        if(!response.ok){
-            setError(json.error)
-        }
-        if(response.ok) {
-            setTitle('')
-            setUser('')
-            setTag('')
-            setBody('')
-            setError(null)
-            console.log('new post added.', json)
-            dispatch({type: 'CREATE_POST', payload: json})
-        }
+    if (!response.ok) {
+      setError(json.error);
     }
+    if (response.ok) {
+      setTitle("");
+      setUser("");
+      setTag("");
+      setBody("");
+      setError(null);
+      console.log("new post added.", json);
+      dispatch({ type: "CREATE_POST", payload: json });
+    }
+  };
 
-    return(
-        <form className='create' onSubmit={formSubmit}>
-            <h3>Add a new Post</h3>
-            
-            <label>Title: </label>
-            <input 
-                type="text"
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-            />
-              
-            <label>User: </label>
-            <input 
-                type="text"
-                onChange={(e) => setUser(e.target.value)}
-                value={user}
-            />
-            
-            <label>Tag: </label>
-            <input 
-                type="text"
-                onChange={(e) => setTag(e.target.value)}
-                value={tag}
-            />
+  return (
+    <form className="create" onSubmit={formSubmit}>
+      <h3>Add a new Post</h3>
 
-            <label>Body: </label>
-            <input 
-                type="text"
-                onChange={(e) => setBody(e.target.value)}
-                value={body}
-            />
+      <label>Title: </label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
 
-            <button> add post </button>
-            
-            {error && <div className="error">All fields need to be filled.</div>}
-        </form>
+      <label>User: </label>
+      <input
+        type="text"
+        onChange={(e) => setUser(e.target.value)}
+        value={user}
+      />
 
+      <label>Tag: </label>
+      <input type="text" onChange={(e) => setTag(e.target.value)} value={tag} />
 
-    )
-}
+      <label>Body: </label>
+      <input
+        type="text"
+        onChange={(e) => setBody(e.target.value)}
+        value={body}
+      />
 
+      <button> add post </button>
 
-export default PostingForm
+      {error && <div className="error">All fields need to be filled.</div>}
+    </form>
+  );
+};
+
+export default PostingForm;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,36 +1,34 @@
-import { useEffect }from 'react'
-import { usePostsContext } from '../hooks/usePostsContext'
+import { useEffect } from "react";
+import { usePostsContext } from "../hooks/usePostsContext";
 
-import PostDetails from "../components/postDetails"
-import PostingForm from '../components/postingForm'
-
+import PostDetails from "../components/postDetails";
+import PostingForm from "../components/postingForm";
 
 const Home = () => {
-   const {posts, dispatch} = usePostsContext()
+  const { posts, dispatch } = usePostsContext();
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const response = await fetch ('https://compiled-mern-hszc.vercel.app/api/posts')
-            const json = await response.json()
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("https://poggers-drab.vercel.app/api/posts");
+      const json = await response.json();
 
-            if(response.ok) {
-                dispatch({type: 'SET_POSTS', payload: json})
-            }
-        }
+      if (response.ok) {
+        dispatch({ type: "SET_POSTS", payload: json });
+      }
+    };
 
-        fetchPosts()
-    }, [])
+    fetchPosts();
+  }, []);
 
-    return(
-        <div className="posts-container">
-            <div className="workouts">
-                {posts && posts.map((post)=> (
-                    <PostDetails key={post._id} post={post}/>
-                ))}
-            </div>
-            <PostingForm/>
-        </div>
-    )
-}
+  return (
+    <div className="posts-container">
+      <div className="workouts">
+        {posts &&
+          posts.map((post) => <PostDetails key={post._id} post={post} />)}
+      </div>
+      <PostingForm />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
